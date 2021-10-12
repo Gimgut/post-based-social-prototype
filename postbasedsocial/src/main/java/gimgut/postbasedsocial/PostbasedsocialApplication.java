372f4f98cbc356d98efe72dbcda98a09344de910
Gimgut <90283957+Gimgut@users.noreply.github.com>
2021-10-12 13:39:40 +0300

    Empty project

---
+++ postbasedsocial/src/main/java/gimgut/postbasedsocial/PostbasedsocialApplication.java
@@ -0,0 +1,13 @@
+package gimgut.postbasedsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostbasedsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostbasedsocialApplication.class, args);
+	}
+
+}
---
+++ postbasedsocial/src/test/java/gimgut/postbasedsocial/PostbasedsocialApplicationTests.java
@@ -0,0 +1,13 @@
+package gimgut.postbasedsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostbasedsocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
