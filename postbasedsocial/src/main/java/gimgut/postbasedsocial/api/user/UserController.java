c7f50a76308ed6f66e2b54e94387abf0dc72f3e1
Gimgut <90283957+Gimgut@users.noreply.github.com>
2021-10-31 18:27:20 +0200

    User controller. Get user

---
+++ postbasedsocial/src/main/java/gimgut/postbasedsocial/api/user/UserController.java
@@ -0,0 +1,34 @@
+package gimgut.postbasedsocial.api.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    private final UserInfoRepository userInfoRepository;
+    private final UserInfoMapper userInfoMapper;
+
+    public UserController(UserInfoRepository userInfoRepository, UserInfoMapper userInfoMapper) {
+        this.userInfoRepository = userInfoRepository;
+        this.userInfoMapper = userInfoMapper;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity getUserById(@PathVariable @NotNull Long id) {
+        Optional<UserInfo> userInfo = userInfoRepository.findById(id);
+        if (!userInfo.isPresent()) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        UserInfoDto userDto = userInfoMapper.toUserInfoDto(userInfo.get());
+        return new ResponseEntity(userDto, HttpStatus.OK);
+    }
+}
