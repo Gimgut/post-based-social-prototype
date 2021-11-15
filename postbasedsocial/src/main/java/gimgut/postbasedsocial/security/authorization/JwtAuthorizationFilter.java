074719a2c050ca0bedc2e7d15d7d6a0dcc0192d9
Gimgut <90283957+Gimgut@users.noreply.github.com>
2021-11-15 15:46:23 +0200

    Refactor JwtAuthorizationFilter

--- postbasedsocial/src/main/java/gimgut/postbasedsocial/security/authorization/JwtAuthorizationFilter.java
+++ postbasedsocial/src/main/java/gimgut/postbasedsocial/security/authorization/JwtAuthorizationFilter.java
@@ -30,52 +30,57 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        logger.info("Enter JwtAuthorizationFilter...");
-        logger.info("Request url: " + request.getRequestURL()
-                + " from remote addr: " + request.getRemoteAddr()
-                + " header X-FORWARDED-FOR" + request.getHeader("X-FORWARDED-FOR"));
+        String forwardedIp = request.getHeader("X-FORWARDED-FOR");
+        String userRequestInfo =
+                "url: "
+                + request.getRequestURL()
+                + " addr: " + (forwardedIp == null ? request.getRemoteAddr() : forwardedIp);
         if (!request.getRemoteAddr().equals("0:0:0:0:0:0:0:1")) {
             response.setStatus(403);
-            logger.info("KICKED OUT");
+            logger.info("KICKED OUT." + userRequestInfo);
             return;
         }
         String servletPath = request.getServletPath();
-        if (
-                !servletPath.startsWith("/api")
+        if (!servletPath.startsWith("/api")
                 || servletPath.startsWith(AUTH_LOGIN_URL)
                 || servletPath.startsWith(REFRESH_TOKEN_URL)) {
-            logger.info("Skipping JwtAuthorizationFilter because path is " + servletPath);
+            logger.info("Skip autho." + userRequestInfo);
             filterChain.doFilter(request, response);
-        } else {
-            logger.info("Try authorization...");
-            String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-                DecodedJWT decodedJWT;
-                try {
-                    String accessToken = authorizationHeader.substring(7);
-                    decodedJWT = jwtService.verifyAccessToken(accessToken);
-                } catch (Exception e) {
-                    logger.info("Authorization failed");
-                    response.setHeader("error", "Authorization failed");
-                    response.sendError(HttpServletResponse.SC_FORBIDDEN);
-                    return;
-                }
-                Long userInfoId = decodedJWT.getClaim("uiid").asLong();
-                String userRole = decodedJWT.getClaim("role").asString();
-                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
-                        new UsernamePasswordAuthenticationToken(userInfoId, null, Arrays.asList(Roles.valueOf(userRole)));
-
-                //Suggestion from spring security doc because of race condition with the following implementation: SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-                SecurityContext context = SecurityContextHolder.createEmptyContext();
-                context.setAuthentication(usernamePasswordAuthenticationToken);
-                SecurityContextHolder.setContext(context);
-                logger.info("Authorization is successful for user uiid: " + userInfoId + " with role: " + userRole);
-
-                filterChain.doFilter(request, response);
-            } else {
-                logger.info("Unauthorized access");
-                filterChain.doFilter(request, response);
-            }
+            return;
         }
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            logger.info("Unautho access." + userRequestInfo);
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        DecodedJWT decodedJWT;
+        try {
+            String accessToken = authorizationHeader.substring(7);
+            decodedJWT = jwtService.verifyAccessToken(accessToken);
+        } catch (Exception e) {
+            logger.info("Autho failed." + userRequestInfo);
+            response.setHeader("error", "Autho failed");
+            response.sendError(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
+        Long userInfoId = decodedJWT.getClaim("uiid").asLong();
+        String userRole = decodedJWT.getClaim("role").asString();
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(
+                        userInfoId,
+                        null,
+                        Arrays.asList(Roles.valueOf(userRole)));
+
+        // Suggestion from spring security doc because of
+        // a race condition with the following implementation:
+        // SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(usernamePasswordAuthenticationToken);
+        SecurityContextHolder.setContext(context);
+        logger.info("Autho success." + " UIID: " + userInfoId + "." + userRequestInfo );
+        filterChain.doFilter(request, response);
     }
 }
